1fd3f017898d3a0da356cdd4fefda712e47fed20
Eli <elimaynard923@gmail.com>
2020-07-15 14:32:11 -0700

    init commit

---
+++ wrongish.js
@@ -0,0 +1,92 @@
+
+const ex = module.exports;
+
+const $or        = ex.$or        = ex.or        = Symbol('$or');
+const $and       = ex.$and       = ex.and       = Symbol('$and');
+const $map       = ex.$map       = ex.map       = Symbol('$map');
+const $pipe      = ex.$pipe      = ex.pipe      = Symbol('$pipe');
+const $with      = ex.$with      = ex.with      = Symbol('$with');
+const $some      = ex.$some      = ex.some      = Symbol('$some');
+const $minus     = ex.$minus     = ex.minus     = Symbol('$minus');
+const $union     = ex.$union     = ex.union     = Symbol('$union');
+const $every     = ex.$every     = ex.every     = Symbol('$every');
+const $filter    = ex.$filter    = ex.filter    = Symbol('$filter');
+const $extend    = ex.$extend    = ex.extend    = Symbol('$extend');
+const $without   = ex.$without   = ex.without   = Symbol('$without');
+const $intersect = ex.$intersect = ex.intersect = Symbol('$intersect');
+const $mapfilter = ex.$mapfilter = ex.mapfilter = Symbol('$mapfilter');
+
+function patch(type, name, aliases, func) {
+  const props = [name, ...aliases];
+  for (const prop of props)
+    Object.defineProperty(type.prototype, prop, {
+      enumerable: false,
+      value: func,
+    });
+}
+
+
+
+
+// == Object == //
+
+patch(Object, $pipe, [], function(func) {
+  return func(this);
+});
+
+
+// == Array == //
+
+patch(Array, $mapfilter, [], function(mapper) {
+  return this.map(mapper).filter(v => v !== false);
+});
+
+patch(Array, $extend, [], function(other) {
+  this.push(...other);
+});
+
+
+// == Set == //
+
+patch(Set, $map, [], function(mapper) {
+  return new Set([...this].map(mapper));
+});
+
+patch(Set, $filter, [], function(pred) {
+  return new Set([...this].filter(pred));
+});
+
+patch(Set, $mapfilter, [], function(mapper) {
+  return this[$map](mapper)[$filter](v => v !== false);
+});
+
+patch(Set, $minus, [], function(other) {
+  return this[$filter](x => !other.has(x));
+});
+
+patch(Set, $intersect, [$and], function(other) {
+  if (this.size >= other.size)
+    return this[$filter](x => other.has(x));
+  else
+    return other[$filter](x => this.has(x));
+});
+
+patch(Set, $union, [$or], function(other) {
+  return new Set([...this, ...other]);
+});
+
+patch(Set, $with, [], function(item) {
+  return new Set([...this, item]);
+});
+
+patch(Set, $without, [], function(item) {
+  return this[$filter](x => x !== item);
+});
+
+patch(Set, $some, [], function(pred) {
+  return [...this].some(pred);
+});
+
+patch(Set, $every, [], function(pred) {
+  return [...this].every(pred);
+});
---
+++ wrongish.test.js
@@ -0,0 +1,82 @@
+
+const $ = require('./wrongish');
+
+const expect = require('expect');
+
+describe('wrongish', () => {
+
+  it('Array[$mapfilter]', () => {
+    const items = [1, 2, 3, 4, 5];
+    const result = items[$.mapfilter](x => x % 2 === 0 && Math.pow(x, 2));
+    expect(result).toEqual([4, 16]);
+  });
+
+  it('Set[$map]', () => {
+    const items = new Set([1, 2, 3, 4, 5]);
+    const result = items[$.map](x => Math.pow(x, 2));
+    expect(result).toEqual(new Set([1, 4, 9, 16, 25]));
+  });
+  
+  it('Set[$filter]', () => {
+    const items = new Set([1, 2, 3, 4, 5]);
+    const result = items[$.filter](x => x % 2 === 0);
+    expect(result).toEqual(new Set([2, 4]));
+  });
+  
+  it('Set[$mapfilter]', () => {
+    const items = new Set([1, 2, 3, 4, 5]);
+    const result = items[$.mapfilter](x => x % 2 === 0 && Math.pow(x, 2));
+    expect(result).toEqual(new Set([4, 16]));
+  });
+
+  it('Set[$minus]', () => {
+    const A = new Set(['A', 'both']);
+    const B = new Set(['B', 'both']);
+    expect(A[$.minus](B)).toEqual(new Set(['A']));
+  });
+  
+  it('Set[$intersect]', () => {
+    const A = new Set(['A', 'both']);
+    const B = new Set(['B', 'both']);
+    expect(A[$.intersect](B)).toEqual(new Set(['both']));
+  });
+  
+  it('Set[$union]', () => {
+    const A = new Set(['A', 'both']);
+    const B = new Set(['B', 'both']);
+    expect(A[$.union](B)).toEqual(new Set(['A', 'both', 'B']));
+  });
+  
+  it('Set[$with]', () => {
+    const S = new Set(['a']);
+    expect(S[$.with]('b')).toEqual(new Set(['a', 'b']));
+  });
+  
+  it('Set[$without]', () => {
+    const S = new Set(['a', 'b']);
+    expect(S[$.without]('b')).toEqual(new Set(['a']));
+  });
+  
+  it('Set[$some]', () => {
+    const A = new Set([1, 2, 3, 4, 5]);
+    expect(A[$.some](x => x > 4)).toBe(true);
+    expect(A[$.some](x => x > 5)).toBe(false);
+  });
+  
+  it('Set[$every]', () => {
+    const A = new Set([1, 2, 3, 4, 5]);
+    expect(A[$.every](x => x > 1)).toBe(false);
+    expect(A[$.every](x => x > 0)).toBe(true);
+  });
+  
+
+  it('Object[$pipe]', () => {
+    const obj = { x: 2, y: 4 };
+    const result = obj[$.pipe](({ x, y }) => x * y);
+    expect(result).toBe(8);
+  });
+  
+});
+
+
+
